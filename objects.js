178697a62747328790a800738a556d1570449e7a
KirishaK <136303274+KirishaK@users.noreply.github.com>
2024-07-03 17:43:32 +0530

    Delete objects.js

--- objects.js
+++
@@ -1,9 +0,0 @@
-let person = {
-    firstName: "kirisha",
-    lastName: "navam",
-    age: 23,
-    fullName: function() {
-        return this.firstName + " " + this.lastName;
-    }
-};
-console.log(person.fullName());  
