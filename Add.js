ff76c1df824c23285421f60b9232146809f3e752
KirishaK <136303274+KirishaK@users.noreply.github.com>
2024-07-03 17:31:31 +0530

    Add files via upload

---
+++ Add.js
@@ -0,0 +1,4 @@
+const addTwoNumbers = (n1, n2) => { 
+    return n1 + n2;
+ } 
+console.log(addTwoNumbers(2, 3));
---
+++ sumArray.js
@@ -0,0 +1,1 @@
+const arr = [1, 2, 3, 4, 5]; const sum = arr.reduce((total, num) => total + num, 0); console.log(sum);
